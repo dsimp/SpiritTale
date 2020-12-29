96d4369c22b48421bc04908b733e6f053df25661
dsimp <55302995+dsimp@users.noreply.github.com>
2020-12-29 10:32:24 -0600

    slight cube img alt naming

--- src/Components/CubeView.js
+++ src/Components/CubeView.js
@@ -24,22 +24,22 @@ const CubeView = ({strDrink, strDrinkThumb}) => {
         <div className= {styles.wrapper}>
             <div className= {styles.area} >
                 <div className= {styles.front} style={mystyle}>
-                    <img src={strDrinkThumb} className= {styles.drinkpic} alt/>
+                    <img src={strDrinkThumb} className= {styles.drinkpic} alt= "front cube img"/>
                 </div>
                 <div className= {styles.back} style={mystyle}>
-                    <img src={strDrinkThumb} className= {styles.drinkpic}/>
+                    <img src={strDrinkThumb} className= {styles.drinkpic} alt= "back cube img"/>
                 </div>
                 <div className= {styles.right} style={mystyle}>
-                    <img src={strDrinkThumb} className= {styles.drinkpic} />
+                    <img src={strDrinkThumb} className= {styles.drinkpic} alt= "right cube img" />
                 </div>
                 <div className={styles.left} style={mystyle}>
-                    <img src={strDrinkThumb} className= {styles.drinkpic}/>
+                    <img src={strDrinkThumb} className= {styles.drinkpic} alt= "left cube img"/>
                 </div>
                 <div className= {styles.top} style={mystyle}>
-                    <img src={strDrinkThumb} className= {styles.drinkpic} />
+                    <img src={strDrinkThumb} className= {styles.drinkpic} alt= "top cube img"/>
                 </div>
                 <div className= {styles.bottom} style={mystyle}>
-                    <img src={strDrinkThumb} className= {styles.drinkpic}/>
+                    <img src={strDrinkThumb} className= {styles.drinkpic} alt= "bottom cube img"/>
                 </div>
             </div>
             
